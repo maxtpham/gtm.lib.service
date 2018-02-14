9922bcdfa025ddef97b8f2f35c6416cd88a206f8
thanhptr <thanhptr@gmail.com>
2018-02-14 23:46:45 +0700

    Moved ExpressAuthen to auth module

--- bin/lib/index.js
+++ bin/lib/index.js
@@ -4,7 +4,6 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./Controller"));
-__export(require("./ExpressAuthen"));
 __export(require("./MongoClient"));
 __export(require("./Service"));
 __export(require("./StaticFile"));
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,4 @@
 export * from "./Controller";
-export * from "./ExpressAuthen";
 export * from "./MongoClient";
 export * from "./Service";
 export * from "./StaticFile";
